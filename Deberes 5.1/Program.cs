d4e1a2eb91a733d38b64b35a3e6328907d9fff8d
BoyVlad <boyko.vladislav.6110@gmail.com>
2021-06-06 22:58:22 +0200

    toTryPush

---
+++ Deberes 5.1/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections;
+
+namespace Deberes_5._1
+{
+    class Employee
+    {
+        string nombre;
+        string apellido;
+        string position;
+        public void AddEmployee()
+        {
+            Console.WriteLine("Введите имя сотрудника: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Введите фамилию сотрудника: ");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Введите должность сотрудника: ");
+            position = Console.ReadLine();
+        }
+        public Employee(string nombre, string apellido, string position)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.position = position;
+        }
+        public Employee()
+        {
+            this.nombre = "Имя";
+            this.apellido = "Фамилия";
+            this.position = "Должность";
+        }
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+            set
+            {
+                apellido = value;
+            }
+        }
+        public string Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("Имя: {0} Фамилия: {1} Должность: {2}", nombre, apellido, position);
+        }
+    }
+
+    class Firma : IEnumerable, IEnumerator
+    {
+
+        private Employee[] obj;
+        private string name;
+        int current = -1;
+        public Firma()
+        {
+            name = "Партнеры+";
+            obj = new Employee[3];
+            obj[0] = new Employee("Хосе", "Андриа", "Редактор");
+            obj[1] = new Employee("Хуан", "Рики", "Клинер");
+            obj[2] = new Employee("Мария", "Гарсия", "Директор");
+        }
+
+        public Firma(string name, int pos)
+        {
+            this.name = name;
+            obj = new Employee[pos];
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i] = new Employee();
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void AddEmployee()
+        {
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i].AddEmployee();
+            }
+        }
+        public float QuantityOfEmployee
+        {
+            get
+            {
+                return obj.Length;
+            }
+        }
+        public void ShowEmployee()
+        {
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i].Show();
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+        public void Reset()
+        {
+            current = -1;
+        }
+        public object Current
+        {
+            get
+            {
+                return obj[current];
+            }
+        }
+        public bool MoveNext()
+        {
+
+            if (current < obj.Length - 1)
+            {
+                current++;
+                return true;
+            }
+            return false;
+        }
+        public Firma Copy()
+        {
+            return new Firma(Name, obj.Length);
+        }
+
+        public void Interacting(int index)
+        {
+            object [] newArray = new object[obj.Length - 1];
+            for (int i = 0; i < index; i++)
+                newArray[i] = obj[i];
+            for (int i = index; i < obj.Length-1; i++)
+                newArray[i] = obj[i+1];
+            obj = null;
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                //obj[i] = newArray[i]; это не работает!!!
+            }
+        }
+        public void Interacting()
+        {
+            obj = new Employee[obj.Length+1];
+            obj[obj.Length] = new Employee();
+            obj[obj.Length].AddEmployee();
+        }
+    } 
+        class Program
+        {
+            public static void Main()
+            {
+                try
+                {
+                    bool end = false;
+                    string cmd;
+                    int Quantity;
+                    Console.WriteLine("Введите навание фирмы: ");
+                    string NameFirma = Console.ReadLine();
+                    //do
+                    //{
+                        Console.WriteLine("Введите количество сотрудников: ");
+                        Quantity = Convert.ToInt32(Console.ReadLine());
+                        /*if (Quantity == 0)
+                        {
+                            Console.WriteLine("Некорректнное количество работников");
+                        }*/
+                    //}
+                    //while (Quantity == 0);
+                    Firma firm = new Firma(NameFirma, Quantity);
+                    firm.AddEmployee();
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Фирма <<{0}>>. Количество сотрудников - {1}", NameFirma, firm.QuantityOfEmployee);
+                        Console.WriteLine("Введите команду(help для вызова справки): ");
+                        cmd = Console.ReadLine();
+                        switch (cmd)
+                        {
+                            case "Добавить сотрудника":
+                                firm.Interacting();
+                                break;
+                            case "Удалить сотрудника":
+                                foreach (Employee b in firm)
+                                {
+                                    int ind = 1;
+                                    Console.Write(ind + ". ");
+                                    ind++;
+                                    b.Show();
+                                }
+                                Console.WriteLine("Сотрудника под каким индексом удалить?");
+                                int del = Convert.ToInt32(Console.ReadLine());
+                                if (del <= 0&&del>firm.QuantityOfEmployee)
+                                {
+                                    Console.WriteLine("Некорректнный индекс работника");
+                                    break;
+                                }
+                                del--;
+                                firm.Interacting(del);
+                                Quantity--;
+                                break;
+                            case "Список сотрудников":
+                                foreach (Employee b in firm)
+                                {
+                                    b.Show();
+                                }
+                                firm.Reset();
+                                break;
+                        }
+
+                    }
+                    while (!end);
+                    foreach (Employee b1 in firm)
+                    {
+                        b1.Show();
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.Read();
+            }
+        }
+    }
+
+
---
+++ Deberes 5.2/Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+class Employee
+{
+	string nombre;
+	string apellido;
+	string position;
+	public Employee(string nombre, string apellido, string position)
+	{
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.position = position;
+	}
+	public Employee()
+	{
+		this.nombre = "Имя";
+		this.apellido = "Фамилия";
+		this.position = "Должность";
+	}
+	public string Nombre
+	{
+		get
+		{
+			return nombre;
+		}
+		set
+		{
+			nombre = value;
+		}
+	}
+	public string Apellido
+	{
+		get
+		{
+			return apellido;
+		}
+		set
+		{
+			apellido = value;
+		}
+	}
+	public string Position
+	{
+		get
+		{
+			return position;
+		}
+		set
+		{
+			position = value;
+		}
+	}
+	public void Show()
+	{
+		Console.WriteLine("Имя: {0} Фамилия: {1} Должность: {2}", nombre, apellido, position);
+	}
+}
+class Firma
+{
+	string nombre;
+	string apellido;
+	string position;
+	List<Employee> Employees { get; }
+	List<string> post = new List<string>();
+	public int Quantity;
+	private string name;
+	public Firma()
+	{
+		name = "Название Фирмы";
+		Employees.Add(new Employee("Иван", "Иванов", "Редактор"));
+		Employees.Add(new Employee("Иванна", "Иванова", "НеРедактор"));
+		Employees.Add(new Employee("Иванна", "Иванова", "Директор"));
+	}
+	public Firma(string name, int Quantity)
+	{
+		Employees = new List<Employee>();
+		this.name = name;
+		this.Quantity = Quantity;
+
+		for (int i = 0; i < Quantity; i++)
+		{
+			Console.WriteLine("Введите имя сотрудника: ");
+			nombre = Console.ReadLine();
+			Console.WriteLine("Введите фамилию сотрудника: ");
+			apellido = Console.ReadLine();
+			Console.WriteLine("Введите должность сотрудника: ");
+			position = Console.ReadLine();
+			Employees.Add(new Employee(nombre, apellido, position)); ;
+		}
+	}
+	public void Add(string nombre, string apellido, string position)
+	{
+		Employees.Add(new Employee(nombre, apellido, position));
+		CheckPosition(position);
+	}
+	public void Del()
+	{
+		int del = 0;
+		int ind = 1;
+		foreach (Employee b in Employees)
+		{
+			Console.Write(ind + ". ");
+			b.Show();
+			ind++;
+		}
+		Console.WriteLine("Введите индекс сотрудника, которого нужно удалить: ");
+		do
+		{
+			del = Convert.ToInt32(Console.ReadLine());
+			if (del == 0 && del > QuantityOfEmployee)
+			{
+				Console.WriteLine("Неверный индекс");
+			}
+		}
+		while (del == 0 && del > QuantityOfEmployee);
+		del--;
+		Employees.RemoveAt(del);
+	}
+	public virtual int QuantityOfEmployee
+	{
+		get
+		{
+			return Employees.Count;
+		}
+	}
+	public string Name
+	{
+		get
+		{
+			return name;
+		}
+	}
+	public void ResConsole()
+	{
+		Console.Clear();
+		ShowList();
+		Console.ReadLine();
+	}
+	public void ShowList()
+	{
+		for (int i = 0; i < Quantity; i++)
+		{
+			Console.Write(i + 1 + ". ");
+			Employees[i].Show();
+		}
+	}
+	public void CheckListPosition()
+	{
+		for (int i = 0; i < QuantityOfEmployee; i++)
+			CheckPosition(Employees[i].Position);
+	}
+	public void CheckPosition(string Position)
+	{
+		if (!post.Contains(Position))
+			AddPosition(Position);
+	}
+	public void AddPosition(string Position)
+	{
+		int indPost;
+		Console.WriteLine("Должности {0} нет в списке", Position);
+		Console.ReadLine();
+		if (post.Count == 0)
+			post.Add(Position);
+		else
+		{
+			for (int i = 0; i < post.Count; i++)
+				Console.WriteLine("{0}. {1}", i + 1, post[i]);
+
+			Console.WriteLine("Введите цифру, на котором месте по иерархии дожна находиться должность {0}(должность на этой позиции сместиться вниз)", Position);
+
+			do
+				indPost = Convert.ToInt32(Console.ReadLine());
+			while (indPost == 0 && indPost > post.Count);
+			indPost--;
+			post.Insert(indPost, Position);
+
+		}
+	}
+	public void СomparisonPosition(int A, int B)
+	{
+		string PositionA, PositionB, NameA, NameB, ApellidoA, ApellidoB;
+		PositionA = Employees[A].Position;
+		PositionB = Employees[B].Position;
+		NameA = Employees[A].Nombre;
+		NameB = Employees[B].Nombre;
+		ApellidoA = Employees[A].Apellido;
+		ApellidoB = Employees[B].Apellido;
+		int IndexA = post.IndexOf(PositionA);
+		int IndexB = post.IndexOf(PositionB);
+		if (IndexA > IndexB)
+			Console.WriteLine("Сотрудник {0} {1} на должности {2} имеет более низкую должноть чем сотрудник {3} {4} на должности {5}", ApellidoA, NameA, PositionA, ApellidoB, NameB, PositionB);
+		if (IndexA < IndexB)
+			Console.WriteLine("Сотрудник {0} {1} на должности {2} имеет более высокую должноть чем сотрудник {3} {4} на должности {5}", ApellidoA, NameA, PositionA, ApellidoB, NameB, PositionB);
+		if (IndexA == IndexB)
+			Console.WriteLine("Сотрудник {0} {1} на должности {2} имеет туже должноть, что и сотрудник {3} {4}", ApellidoA, NameA, PositionA, ApellidoB, NameB, PositionB);
+	}
+}
+class Program
+{ 
+	static void Main()
+	{
+
+		try
+		{
+			bool end = false;
+			string cmd;
+			Console.WriteLine("Введите навание фирмы: ");
+			string NameFirma = Console.ReadLine();
+			Console.WriteLine("Введите количество сотрудников: ");
+			int Quantity = Convert.ToInt32(Console.ReadLine());
+			Firma firm = new Firma(NameFirma, Quantity);
+			firm.CheckListPosition();
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Фирма <<{0}>>. Количество сотрудников - {1}", firm.Name, firm.QuantityOfEmployee);
+				Console.WriteLine("Введите команду(help для вызова справки): ");
+				cmd = Console.ReadLine();
+				switch (cmd)
+				{
+					case "Добавить сотрудника":
+						Console.WriteLine("Введите имя сотрудника: ");
+						string nombre = Console.ReadLine();
+						Console.WriteLine("Введите фамилию сотрудника: ");
+						string apellido = Console.ReadLine();
+						Console.WriteLine("Введите должность сотрудника: ");
+						string position = Console.ReadLine();
+						firm.Add(nombre, apellido, position);
+						firm.ResConsole();
+						break;
+					case "Удалить сотрудника":
+						Quantity--;
+						firm.Del();
+						firm.ResConsole();
+						break;
+					case "Список сотрудников":
+						firm.ResConsole();
+						break;
+					case "Проверить по должности":
+						int A, B;
+						firm.ResConsole();
+						Console.WriteLine("Введите индексы тех, кого вы хотите сравнить. Первый: ");
+						do
+						{
+							A = Convert.ToInt32(Console.ReadLine());
+							if (A == 0 && A > firm.QuantityOfEmployee)
+								Console.WriteLine("Недопустимый индекс");
+						}
+						while (A == 0 && A > firm.QuantityOfEmployee);
+						Console.WriteLine("Второй: ");
+						do
+						{
+							B = Convert.ToInt32(Console.ReadLine());
+							if (B == 0 && B > firm.QuantityOfEmployee)
+								Console.WriteLine("Недопустимый индекс");
+						}
+						while (B == 0 && B > firm.QuantityOfEmployee);
+						A--;
+						B--;
+						firm.СomparisonPosition(A, B);
+						break;
+					case "Конец":
+						end = true;
+						break;
+					case "help":
+                        Console.WriteLine("Добавить сотрудника, Удалить сотрудника, Список сотрудников, Проверить по должности, Конец");
+						break;
+				}
+				Console.ReadLine();
+			}
+			while (!end);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		Console.Read();
+	}
+}

3e6465a2e3e29bd98172867d0ca9c9bb1c0c15c0
BoyVlad <boyko.vladislav.6110@gmail.com>
2021-06-16 22:38:44 +0200

    16.06

---
+++ Deberes 6.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSharpApplication.SearchInFiles
+{
+    class SearchDirectory
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Введите путь в каталог:");
+            string catalog = Console.ReadLine();
+            Console.Write("Введите маску для файлов: ");
+            string Mask = Console.ReadLine();
+
+            if (catalog[catalog.Length - 1] != '\\')
+                catalog += '\\';
+
+            DirectoryInfo di = new DirectoryInfo(catalog);
+            if (!di.Exists)
+            {
+                Console.WriteLine("Каталога с таким именем не существует или он не может быть обработан програмой");
+                return;
+            }
+
+            Search(catalog, Mask, catalog + "ResultSearch.txt");
+        }
+        private static void Search(string catalog, string Mask, string ResultSearch)
+        {
+            StreamWriter sw = new StreamWriter(ResultSearch, true);
+
+            try
+            {
+                foreach (string Path in Directory.GetFiles(catalog, Mask, SearchOption.AllDirectories))
+                {
+                    Console.WriteLine(Path);
+                    DateTime lastred = File.GetLastWriteTime(Path);
+                    sw.WriteLine(Path + " в последний раз редактирован " + lastred);    
+                }
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка при работе с {0}", catalog);
+                sw.Close();
+                return;
+            }
+        }
+    }
+}
+
---
+++ Deberes 6.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Deberes_6._2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Введите путь в каталог:");
+            string catalog = Console.ReadLine();
+            if (catalog.Length == 0)
+                catalog = "C:\\Users\\usuario\\Downloads\\Test";
+            Console.Write("Введите маску для файлов: ");
+            string Mask = Console.ReadLine();
+            if (Mask.Length == 0)
+                Mask = "test.txt";
+            Console.WriteLine("Введите текст, который нужно заменить: ");
+            string Text = Console.ReadLine();
+            if (Text.Length == 0)
+                Text = "text";
+            Console.WriteLine("Введите текст для замены: ");
+            string NewText = Console.ReadLine();
+            if (NewText.Length == 0)
+                NewText = "THIS IS TEXT!";
+            DirectoryInfo di = new DirectoryInfo(catalog);
+            if (catalog[catalog.Length - 1] != '\\')
+                catalog += '\\';
+
+            if (!di.Exists)
+            {
+                Console.WriteLine("Каталога с таким именем не существует или он не может быть обработан програмой");
+                return;
+            }
+            Search(catalog, Mask, Text, NewText);
+        }
+        private static void Search(string catalog, string Mask, string Text, string NewText)
+        {
+            StreamReader sr = null;
+            DirectoryInfo di = new DirectoryInfo(catalog);
+            foreach (string Path in Directory.GetFiles(catalog, Mask, SearchOption.AllDirectories))
+            {
+                sr = new StreamReader(Path);
+                Console.WriteLine("Поиск текста в " + Path);
+                string Content = sr.ReadToEnd();
+                string New = Content.Replace(Text, NewText);
+                if (New==Content)
+                    Console.WriteLine("В файле не найдено текста для замены");
+                sr.Close();
+                StreamWriter sw = new StreamWriter(Path, false, Encoding.Default);
+                sw.Write(New);
+                sw.Close();
+            }
+        }
+    }
+}
